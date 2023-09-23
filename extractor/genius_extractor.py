4d2881ebe04894c3c25479c73b1f072111f36e8e
chsantos234 <camilosantos1709@gmail.com>
2023-09-23 20:10:03 -0300

    feat: extractor

--- extractor/genius_extractor.py
+++ extractor/genius_extractor.py
@@ -2,7 +2,11 @@ import http.client
 from env import env
 import json
 
-def extractor(requesType):
+def extractor(clientResponse):
+
+    responseList = clientResponse.split(',')
+    requestType,param = responseList[0],responseList[1]
+
     conn = http.client.HTTPSConnection("genius-song-lyrics1.p.rapidapi.com")
 
     headers = {
@@ -11,12 +15,32 @@ def extractor(requesType):
     }
 
     request = {
-        "letra": "/song/lyrics/", # precisa: id-musica
-        "pesquisar": "/search/" # precisa: string-formatada
+        "info": "/artist/details/",
+        "álbuns": "/artist/albums/",
+        "música": "/artist/songs/",
+        "letras": "/song/lyrics/"
     }
 
-    url = f'{request}?'
 
-    conn.request()
-    res = conn.getresponse("GET",url,headers=headers)
+    if requestType != "letras":
+
+        newString = param.replace(' ','%20')
+        conn.request("GET",f"/search/?q={newString}&per_page=2&page=1",headers=headers)
+        artistRes = conn.getresponse()
+        artistData = json.loads(artistRes.read().decode("utf-8"))
+        Id = artistData['hits'][0]['result']['primary_artist']['id']
+
+    else:
+        Id = param
+
+    url = f"{request[requestType]}?id={Id}"
+
+    if requestType ==  "letras": url += "&text_format=plain"
+
+    conn.request("GET",url,headers=headers)
+    res = conn.getresponse()
     data = json.loads(res.read().decode("utf-8"))
+
+    
+
+    return data,url
